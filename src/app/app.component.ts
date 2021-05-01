4b1fdd632f86cbe0b0377de89c86fce923d37120
Tazz1919 <tazbaki19191@gmail.com>
2021-05-01 17:52:30 +0200

    Step1 co authorized by: chibounisme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Typewriter from 'typewriter-effect/dist/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'job-offers-management-system-frontend';
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  ngOnInit(){
+    var app = this.document.getElementById('typewriter');
+    var typewriter = new Typewriter(app, {
+      loop: true,
+      delay: 75,
+    });
+    
+    typewriter
+      .pauseFor(2500)
+      .typeString('welcome to our website')
+      .pauseFor(300)
+      .pauseFor(1000)
+      .start();
+  }
 }
+
+
