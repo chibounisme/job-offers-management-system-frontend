4c1c2189a7e3df85eb923a637026f94a5c86e7f8
Tazz1919 <tazbaki19191@gmail.com>
2021-05-11 18:16:23 +0200

    hellopage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Typewriter from 'typewriter-effect/dist/core';
 import { DOCUMENT } from '@angular/common';
 import { Inject } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +12,13 @@ export class AppComponent implements OnInit {
   title = 'job-offers-management-system-frontend';
   constructor(@Inject(DOCUMENT) private document: Document) {}
   ngOnInit(){
+    this.document.querySelector(".navbar-toggler").addEventListener("click",()=>{
+      if (this.document.querySelector(".collapse.navbar-collapse").classList.contains("show")){
+        this.document.querySelector(".collapse.navbar-collapse").classList.remove("show")
+      }
+      else{this.document.querySelector(".collapse.navbar-collapse").classList.add("show")}
+    })
+    AOS.init();
     var app = this.document.getElementById('typewriter');
     var typewriter = new Typewriter(app, {
       loop: true,
