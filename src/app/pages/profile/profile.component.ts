222a9d26c2fe518a472223b0813989b4c9c84926
Mohamed Chiboub <mohamedchiboub97@gmail.com>
2021-05-14 16:09:28 +0100

    mise à jouren

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NotificationsService } from 'angular2-notifications';
-import { FormGroup, FormControl, ValidatorFn, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, ValidatorFn, FormBuilder, AbstractControl, ValidationErrors } from '@angular/forms';
 
 import { Validators } from '@angular/forms';
 @Component({
@@ -12,31 +12,36 @@ import { Validators } from '@angular/forms';
 })
 export class ProfileComponent implements OnInit {
   closeModal: string;
-  options:any={
+
+  options: any = {
     timeOut: 3000,
     showProgressBar: true,
     pauseOnHover: true,
     clickToClose: true
   };
- tags :any =[{name:" amibitieux"},{name:"serieux"}];
- profileForm =this.formBuilder.group({
-  file: new FormControl(''),
-  Nom: new FormControl('',Validators.minLength(3)),
-  Prenom: new FormControl('',Validators.minLength(3)),
-  mdp: new FormControl('',Validators.minLength(8)),
-  cmdp: new FormControl(''),
-  Email: new FormControl('',Validators.email),
-  tel: new FormControl(''),
-  adresse: new FormControl(''),
-  Gouvernorat: new FormControl(''),
- Ville: new FormControl(''),
-  Tags: new FormControl(''),
-},{ validators: this.checkPasswords });
+
+  tags: any = [{ name: " amibitieux" }, { name: "serieux" }];
+
+  profileForm = this.formBuilder.group({
+    file: new FormControl(''),
+    Nom: new FormControl('', Validators.minLength(3)),
+    Prenom: new FormControl('', Validators.minLength(3)),
+    mdp: new FormControl('', Validators.minLength(8)),
+    cmdp: new FormControl('', [Validators.minLength(8), this.checkPassword()]),
+    Email: new FormControl('', Validators.email),
+    tel: new FormControl(''),
+    adresse: new FormControl(''),
+    Gouvernorat: new FormControl(''),
+    Ville: new FormControl(''),
+    Tags: new FormControl(''),
+  });
+
   FormBuilder: any;
-  constructor(private modalService: NgbModal,private _service: NotificationsService, private formBuilder: FormBuilder) {}
+
+  constructor(private modalService: NgbModal, private _service: NotificationsService, private formBuilder: FormBuilder) { }
 
   triggerModal(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((res) => {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((res) => {
       this.closeModal = `Closed with: ${res}`;
     }, (res) => {
       this.closeModal = `Dismissed ${this.getDismissReason(res)}`;
@@ -49,22 +54,38 @@ export class ProfileComponent implements OnInit {
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
     } else {
-      return  `with: ${reason}`;
+      return `with: ${reason}`;
     }
   }
   ngOnInit(): void {
   }
-  created(event: any){};
-  destroyed(event: any){};
-  onclick(){
-    this._service.success("succés");
-    console.warn(this.profileForm);
-    
+  created(event: any) { };
+  destroyed(event: any) { };
+  onclick() {
+    console.log(this.profileForm);
+    if (this.profileForm.valid) {
+      // appel lel backend
+
+      // ken el appel raja3 réponse valide
+      if (true) {
+        this._service.success("succés");
+      } else {
+        // sinon
+        this._service.error("erreur");
+      }
+      this.modalService.dismissAll();
+    }
   }
   checkPasswords(group: FormGroup) { // here we have the 'passwords' group
     const password = group.get('password')?.value;
     const confirmPassword = group.get('confirmPassword')?.value;
-  
-    return password === confirmPassword ? null : { notSame: true }     
+
+    return password === confirmPassword ? null : { notSame: true }
+  }
+  checkPassword(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const cmdp = control.value;
+      return cmdp == this.profileForm?.value?.mdp ? null : { notSame: true };
+    }
   }
 }
