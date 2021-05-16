b2f50fd71b87fb38c98527ad6558be66e11225f0
Mohamed Chiboub <mohamed.chiboub@supcom.tn>
2021-05-16 15:21:43 +0100

    updating

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, ValidationErrors, ValidatorFn, Validators,FormBuilder } from '@angular/forms';
+import Stepper from 'bs-stepper';
 
 @Component({
   selector: 'app-register',
@@ -14,10 +16,16 @@ export class RegisterComponent implements OnInit {
     cmdp: new FormControl('', [Validators.required,Validators.minLength(8), this.checkPassword()]),
   });
 FormBuilder:any;
-  constructor(private formBuilder: FormBuilder) { }
-
+  constructor(private formBuilder: FormBuilder, @Inject(DOCUMENT) private document: Document) { }
+  stepper: Stepper;
   ngOnInit(): void {
+    this.stepper = new Stepper(this.document.querySelector('.bs-stepper'));
   }
+
+  onclick() {
+    this.stepper.next();
+  }
+
   checkPassword(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const cmdp = control.value;
