a203505d1aba38ba49f1a560e9fe0db8a6b3c544
Tazz1919 <tazbaki19191@gmail.com>
2021-05-24 23:41:43 +0200

    up

--- src/app/pages/joboffers/joboffers.component.ts
+++ src/app/pages/joboffers/joboffers.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NotificationsService } from 'angular2-notifications';
 @Component({
   selector: 'app-joboffers',
   templateUrl: './joboffers.component.html',
   styleUrls: ['./joboffers.component.css']
 })
 export class JoboffersComponent implements OnInit {
-
-  constructor() { }
-
+  closeModal: string;
+  constructor(private modalService: NgbModal, private _service: NotificationsService) { }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+  triggerModal(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((res) => {
+      this.closeModal = `Closed with: ${res}`;
+    }, (res) => {
+      this.closeModal = `Dismissed ${this.getDismissReason(res)}`;
+    });
+  }
   ngOnInit(): void {
   }
 
